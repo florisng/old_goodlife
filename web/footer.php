89ef341a8208f7d678c252ffe75d54fdd521810f
Floris Ng <florisngendahayo@gmail.com>
2022-09-21 18:57:07 +0200

    Sept 21st, 2022 - 6:57 PM

--- web/footer.php
+++ web/footer.php
@@ -1,11 +1,12 @@
 <div class="footer">
     <div class="footer_in">
         <div class="footer_right_border">
-            <h2>About us</h2>
+            <h3>About us</h3>
             We have built the foundation of a Professional Pharmacy Health & Beauty chain, we now want East Africans to start living the Goodlife!
+            <img src="./img/other_logo.png" alt="">
         </div>
         <div class="footer_contact_us footer_right_border">
-            <h2>Contact us</h2>
+            <h3>Contact us</h3>
             <img src="./img/address.png" alt="" />Sylverback Mall, Kicukiro - Sonatube
             <p>
                 <img src="./img/phone.png" alt="" />+250 791232266 (Call Center) +250 791232150 (Pharmacy)
@@ -13,11 +14,10 @@
             <img src="./img/mail.png" alt="" /><a href="mailto:info@goodlife.rw">info@goodlife.rw</a>
         </div>
         <div>
-            <h2>Find us on</h2>
+            <h3>Follow us on</h3>
             <a href="https://www.facebook.com" target="_blank" title="Facebook - Goodlife"><img src="./img/fb.png" alt="" /></a>
             <a href="https://www.instagram.com" target="_blank" title="Instagram - Goodlife"><img src="./img/ig.png" alt="" /></a>
             <a href="https://www.twitter.com" target="_blank" title="Twitter - Goodlife"><img src="./img/twi.png" alt="" /></a>
-            <a href="https://www.youtube.com" target="_blank" title="YouTube - Goodlife"><img src="./img/you.png" alt="" /></a>
         </div>
     </div>
     <div class="small_txt">
@@ -38,7 +38,6 @@
         <a href="https://www.facebook.com" target="_blank" title="Facebook - Goodlife"><img src="./img/fb.png" alt="" /></a>
         <a href="https://www.instagram.com" target="_blank" title="Instagram - Goodlife"><img src="./img/ig.png" alt="" /></a>
         <a href="https://www.twitter.com" target="_blank" title="Twitter - Goodlife"><img src="./img/twi.png" alt="" /></a>
-        <a href="https://www.youtube.com" target="_blank" title="YouTube - Goodlife"><img src="./img/you.png" alt="" /></a>
     </div>
     <div class="small_txt">
         © 2022 Goodlife Health & Beauty <br> All right reserved

8d42ab2fea201336033eb42f2576dcba909ba0dc
Floris Ng <florisngendahayo@gmail.com>
2022-09-23 08:09:33 +0200

    Sept 23rd, 2022

--- index.php
+++ index.php
@@ -1,68 +1,215 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="./img/icon.ico">
-    <title>Goodlife Health & Beatuty</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" media="screen">
+    <link rel="stylesheet" type="text/css" href="css/header.css" media="screen">
+    <link rel="stylesheet" type="text/css" href="css/my_content.css" media="screen">
+    <link rel="stylesheet" type="text/css" href="css/slideshow.css" media="screen">
+    <link rel="stylesheet" type="text/css" href="css/footer.css" media="screen">
     <style>
-        body {
-            background-color: #692d91;
-            color: #fff;
-        }
-        img {
-            width: 50%;
-        }
-        .center_text {
-            margin-top: 50px;
-            text-align: center;
-        }
-        input {
-            padding: 10px;
-            border-radius: 10px;
-            border: #fff solid 5px;
-            font-size: 20px;
-            color: #692d91;
-            transition: 1s;
-        }
-        input[type="password"] {
-            width: 40%;
-        }
-        input[type="submit"]:hover {
-            background-color: #692d91;
-            color: #fff;
-            cursor: pointer;
-        }
-        .wrong {
-            background-color: #fff;
-            color: red;
-            padding: 30px;
-        }
+            @import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300&display=swap');
+            body {
+                font-family: 'Roboto Condensed', sans-serif;
+            }
     </style>
+    <title>Goodlife Health & Beauty</title>
 </head>
 <body>
-    <div class="center_text">
-        <img src="./web/img/second_logo.jpg" alt="">
-        <form action="" method="post">
-            <input type="password" name="pass" placeholder="Passcode" required>
-            <p>
-                <input type="submit" name="login" value="Go">
-            </p>
-        </form>
-        <?php
-            if(isset($_POST["login"])) {
-                if($_POST["pass"] == "123abc") {
-                    session_start();
-                    $_SESSION["bravogoodlife"] = true;
-                    header('Location: ./web');
-                } else {
-                    echo "<div class='wrong'>";
-                        echo "Wrong passcode";
-                    echo "</div>";
-                }
-            }
-        ?>
+    <?php include("header.php"); ?>
+    <div class="slideshow-container">
+
+        <div class="mySlides fade">
+            <div class="numbertext">1 / 3</div>
+            <img src="./img/slideshow/j&j.jpg" style="width:100%">
+            <div class="text">Caption Text</div>
+        </div>
+
+        <div class="mySlides fade">
+            <div class="numbertext">2 / 3</div>
+            <img src="./img/slideshow/off.jpg" style="width:100%">
+            <div class="text">Caption Two</div>
+        </div>
+
+        <div class="mySlides fade">
+            <div class="numbertext">3 / 3</div>
+            <img src="./img/slideshow/nivea.jpg" style="width:100%">
+            <div class="text">Caption Three</div>
+        </div>
     </div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+</div>
+
+    <div class="my_content">
+        <div class="services_pics">
+            <div>
+                <span class="services_title_1">Health & Wellness</span>
+                <img src="./img/health.jpg" alt="">
+                <p class="service_text_1">
+                    Lifestyle - Vitamine - Nutrition - Wellness - Body - Beauty - Weight Loss - Calories - Goals - Endurance - Active - Move - Health - Run - Fruits - Focus - Natural - Motivation - Exercice
+                </p>
+            </div>
+            <div>
+                <span class="services_title_2">Simply Beauty</span>
+                <img src="./img/gorgeous.jpg" alt="">
+                <p class="service_text_2">
+                    Cute - Nice - Superb - Sublime - Charming - Lovely - Handsome - Gorgeous - Magnificient - Wonderful - Splendid - Admirable - Elegant - Pleasing - Stunning - Graceful - Pretty
+                </p>
+            </div>
+            <div>
+                <span class="services_title_3">Personal Care</span>
+                <img src="./img/personal_care.jpg" alt="">
+                <p class="service_text_3">
+                    In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.
+                </p>
+            </div>
+        </div>
+        <div class="slogan">
+            "Our Delivery Van 24/7 set to Go"
+        </div>
+        <div class="ambulance">
+            <img src="./img/ambulance.jpg" alt="">
+        </div>
+        <div class="pregnancy">
+            <div>
+                <h1>Your guide to a <span class="purple_color">Healthy pregnancy</span></h1>
+                <p>
+                    To keep you and your baby healthy, it's important that you Don't smoke or drink alcohol, Eat healthy and get enough folic acid, Stay physically active.
+                </p>
+                <p>
+                    Get more tips for a healthy pregnancy. If you just learned that you're pregnant, find out what to do nextThis link is external to health.gov. Check out these tips for staying healthy and safe during pregnancy.
+                </p>
+                <p>
+                    From trying to conceive to the first trimester to labor, learn what to expect during your pregnancy.
+                </p>
+                <hr>
+                <a href="">LEARN MORE</a>
+            </div>
+            <div class="">
+                <img src="./img/pregnant.jpg" alt="">
+            </div>
+        </div>
+        <div class="phone_pregnancy">
+                <h2>Your guide to a <br> <span class="purple_color">Healthy pregnancy</span></h2>
+                <img src="./img/pregnant.jpg" alt="">
+                <p>
+                    To keep you and your baby healthy, it's important that you Don't smoke or drink alcohol, Eat healthy and get enough folic acid, Stay physically active.
+                </p>
+                <p>
+                    Get more tips for a healthy pregnancy. If you just learned that you're pregnant, find out what to do nextThis link is external to health.gov. Check out these tips for staying healthy and safe during pregnancy.
+                </p>
+                <hr>
+                <a href="">LEARN MORE</a>
+        </div>
+        <!-- End -->
+        <h1 class="center_text">Beauty</h1>
+        <!-- Container element -->
+        <div class="parallax_one"></div>
+        <h1 class="center_text">Wigs</h1>
+        <div class="omg_img">
+            <div>
+                <img src="./img/omg/wig_1.jpg" alt=""><br>
+                Straight
+            </div>
+            <div>
+                <img src="./img/omg/wig_2.jpg" alt=""><br>
+                Body Wave
+            </div>
+            <div>
+                <img src="./img/omg/wig_3.jpg" alt=""><br>
+                Deep Wave
+            </div>
+        </div>
+        <div class="omg_img">
+            <div>
+                <img src="./img/omg/wig_5.jpg" alt=""><br>
+                Kinky Straight
+            </div>
+            <div>
+                <img src="./img/omg/wig_6.jpg" alt=""><br>
+                Loose Wave
+            </div>
+            <div>
+                <img src="./img/omg/wig_7.jpg" alt=""><br>
+                Water Wave
+            </div>
+        </div>
+        <div class="omg_img">
+            <div>
+                <img src="./img/omg/3.jpg" alt="">Tootsie
+            </div>
+            <div>
+                <img src="./img/omg/5.jpg" alt="">Domino
+            </div>
+            <div>
+                <img src="./img/omg/6.jpg" alt="">Pumpkin
+            </div>
+            <div>
+                <img src="./img/omg/4.jpg" alt="">Snickerdoodle
+            </div>
+        </div>
+        <h1 class="center_text">Makeup</h1>
+        <!-- Container element -->
+        <div class="parallax_two"></div>
+        <div class="omg_img">
+            <div>
+                <img src="./img/beauty/nails_1.jpg" alt="">
+            </div>
+            <div>
+                <img src="./img/beauty/hair.jpg" alt="">
+            </div>
+            <div>
+                <img src="./img/beauty/pro_1.jpg" alt="">
+            </div>
+        </div>
+        <div class="slogan">
+            When you look good you feel good
+        </div>
+        <!-- Container element -->
+        <div class="parallax_three">
+            <span class="para_three_text">Create your Style <br> With Us</span>
+        </div>
+        <div class="slogan">
+            "We now want East Africans to start living the Goodlife!"
+        </div>
+    </div>
+    <?php include("footer.php"); ?>
+    <script>
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
+
+var prevScrollPos = window.pageYOffset;
+window.onscroll = function() {
+        var currentScrollPos = window.pageYOffset;
+        document.querySelector(".small_header").style.display = prevScrollPos > currentScrollPos ? "block" : "none";
+        prevScrollPos = currentScrollPos;
+}
+
+</script>
 </body>
 </html>

824938dc9c221c9d1f1d72db50c1d83d22400b99
Floris Ng <florisngendahayo@gmail.com>
2022-09-24 16:39:39 +0200

    Sept 24rd, 2022 - 4:38PM

--- index.php
+++ index.php
@@ -53,16 +53,16 @@
         </div>
 
     </div>
-<br>
+    <br>
 
-<div style="text-align:center">
-  <span class="dot"></span> 
-  <span class="dot"></span> 
-  <span class="dot"></span> 
-  <span class="dot"></span>
-  <span class="dot"></span>
-  <span class="dot"></span>
-</div>
+    <div style="text-align:center">
+    <span class="dot"></span> 
+    <span class="dot"></span> 
+    <span class="dot"></span> 
+    <span class="dot"></span>
+    <span class="dot"></span>
+    <span class="dot"></span>
+    </div>
 
     <div class="my_content">
         <div class="services_pics">
@@ -197,33 +197,32 @@
     </div>
     <?php include("footer.php"); ?>
     <script>
-let slideIndex = 0;
-showSlides();
+        let slideIndex = 0;
+        showSlides();
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 4000); // Change image every 3 seconds
-}
+        function showSlides() {
+            let i;
+            let slides = document.getElementsByClassName("mySlides");
+            let dots = document.getElementsByClassName("dot");
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";  
+            }
+            slideIndex++;
+            if (slideIndex > slides.length) {slideIndex = 1}    
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex-1].style.display = "block";  
+            dots[slideIndex-1].className += " active";
+            setTimeout(showSlides, 4000);
+        }
 
-var prevScrollPos = window.pageYOffset;
-window.onscroll = function() {
-        var currentScrollPos = window.pageYOffset;
-        document.querySelector(".small_header").style.display = prevScrollPos > currentScrollPos ? "block" : "none";
-        prevScrollPos = currentScrollPos;
-}
-
-</script>
+        let prevScrollPos = window.pageYOffset;
+        window.onscroll = function() {
+            let currentScrollPos = window.pageYOffset;
+            document.querySelector(".small_header").style.display = prevScrollPos > currentScrollPos ? "block" : "none";
+            prevScrollPos = currentScrollPos;
+        }
+    </script>
 </body>
 </html>
